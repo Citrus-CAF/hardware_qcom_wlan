ea5e37db03e99537f55ae3f7552462786d2fabe8
Ethan Chen <intervigil@gmail.com>
2014-04-14 15:52:41 -0700

    wcnss: Build and dlopen wcnss_qmi_client as a shared library
    * Allows us to decouple QMI from wcnss_service for those devices that
      rely on QMI features for wcnss.
    
    Change-Id: I30a1f9a8760dcd19761d473cedb2104b7b33069c
    
    wcnss_service: Fix build fail when WCNSS_QMI_OSS is defined
    
    Change-Id: I9b14f384901586df5c5e255b15cbeeba20bb6277

--- wcnss-service/Android.mk
+++ wcnss-service/Android.mk
@@ -1,15 +1,20 @@
 ifneq (,$(filter arm aarch64 arm64, $(TARGET_ARCH)))
+
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := wcnss_service
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/common/inc/
 LOCAL_SRC_FILES := wcnss_service.c
 LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog
 ifneq ($(QCPATH),)
+
 ifeq ($(strip $(TARGET_USES_QCOM_WCNSS_QMI)),true)
 ifeq ($(TARGET_USES_WCNSS_MAC_ADDR_REV),true)
 LOCAL_CFLAGS += -DWCNSS_QMI_MAC_ADDR_REV
 endif
+ifneq ($(QCPATH),)
 LOCAL_CFLAGS += -DWCNSS_QMI
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qmi-framework/inc
 LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/qmi/services
@@ -22,7 +27,30 @@ LOCAL_SHARED_LIBRARIES += libmdmdetect
 endif #TARGET_USES_QCOM_WCNSS_QMI
 endif #QCPATH
 LOCAL_CFLAGS += -DDYNAMIC_NV
+
+LOCAL_SHARED_LIBRARIES += libwcnss_qmi
+else
+LOCAL_CFLAGS += -DWCNSS_QMI_OSS
+LOCAL_SHARED_LIBRARIES += libdl
+endif #QCPATH
+LOCAL_CFLAGS += -DDYNAMIC_NV
+endif #TARGET_USES_QCOM_WCNSS_QMI
+
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -Wall -Werror
+LOCAL_CFLAGS += -Wall
+
 include $(BUILD_EXECUTABLE)
-endif
+
+ifeq ($(strip $(TARGET_USES_QCOM_WCNSS_QMI)),true)
+ifneq ($(QCPATH),)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -Wall
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif #QCPATH
+endif #TARGET_USES_QCOM_WCNSS_QMI
+
+endif #TARGET_ARCH == arm
